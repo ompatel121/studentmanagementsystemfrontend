bbcb70bd7d11e129d8dedbea5f417a866bc3f618
Ompatel121 <20it103@charusat.edu.in.com>
2022-09-12 20:45:00 +0530

    Added filter to refresh the table.

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
+import Container from 'react-bootstrap/Container';
+
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
+      <Container>
       <Student/>
       <StudentList/>
-      
+      </Container>
+      <Footer/>
     </div>
   );
 }
